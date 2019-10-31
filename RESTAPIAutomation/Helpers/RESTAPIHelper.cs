664132efa6218a384500f0f712f9f3266db0f7ed
vinay2784 <vinay2study@gmail.com>
2019-10-31 08:18:19 -0400

    base version of automation code

---
+++ RESTAPIAutomation/Helpers/RESTAPIHelper.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RESTAPIAutomation.Helpers
+{
+    public class RESTAPIHelper
+    {
+        public static RestClient restClient;
+        public static RestRequest restRequest;
+        public static string baseUrl = "http://api.zippopotam.us/";
+
+        public static RestClient SetupURL(string endpoint)
+        {
+            var url = Path.Combine(baseUrl, endpoint);
+            return restClient = new RestClient(baseUrl);
+        }
+
+        public static RestRequest CreateRequest()
+        {
+            restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            return restRequest;
+        }
+        public static RestRequest CreateRequestWithResource(string resource)
+        {
+            restRequest = new RestRequest(resource, Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            return restRequest;
+        }
+
+        public static IRestResponse GetRestResponse()
+        {
+            return restClient.Execute(restRequest);
+        }
+    }
+
+}
